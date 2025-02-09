d70001e018ff9d38ecfa9fe2ba0a6f99396728f0
sassan <125366201+emirsassan@users.noreply.github.com>
2025-02-09 14:21:42 +0300

    expose discount api

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { getApiKey, getMode } from './config';
 export { generateTypesFromApi } from './generator';
 export { createCheckoutSession } from './checkout';
 export { activateLicense, deactivateLicense, validateLicense } from './license';
+export { createDiscountCode } from './discount';
